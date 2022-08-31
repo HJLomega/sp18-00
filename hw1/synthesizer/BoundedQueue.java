385f541b2845b748c62a44c58feb1db2ed00ad0e
ljh <2450642014@qq.com>
2022-08-31 16:58:48 +0800

    improve Style

--- hw1/synthesizer/BoundedQueue.java
+++ hw1/synthesizer/BoundedQueue.java
@@ -11,6 +11,11 @@ public interface BoundedQueue<T> {
 
     T peek();           // return (but do not delete) item from the front
 
-    default boolean isEmpty() {return fillCount() == 0;}      // is the buffer empty (fillCount equals zero)?
-    default boolean isFull()  {return fillCount() == capacity();}      // is the buffer full (fillCount is same as capacity)?
+    default boolean isEmpty() {
+        return fillCount() == 0;
+    }      // is the buffer empty (fillCount equals zero)?
+
+    default boolean isFull() {
+        return fillCount() == capacity();
+    }      // is the buffer full (fillCount is same as capacity)?
 }
