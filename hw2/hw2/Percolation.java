a4a1eff5b57e4d988101f1bf302957f0cbf1f1f3
ljh <2450642014@qq.com>
2022-09-10 12:16:48 +0800

    complete Percolation without UF

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -5,44 +5,45 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
 
     private int N;
-    private WeightedQuickUnionUF uf;
-    private int [][] grid;
+    //private WeightedQuickUnionUF uf;
+    private int[][] grid;
     /* 0 : block
        1 : open
        2 : full */
     private int nOfOpenSites;
+
     // create N-by-N grid, with all sites initially blocked
-    public Percolation(int N){
-        if (N <= 0){
+    public Percolation(int N) {
+        if (N <= 0) {
             throw new java.lang.IllegalArgumentException("N must be positive");
         }
         this.N = N;
         nOfOpenSites = 0;
-        uf = new WeightedQuickUnionUF(N*N);
+        //uf = new WeightedQuickUnionUF(N*N);
         grid = new int[N][N];
-        for (int i = 0; i < N; i++){
-            for (int j = 0; j < N; j++){
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 grid[i][j] = 0;
             }
         }
     }
 
-    private int xy2Index(int row, int col){
+    private int xy2Index(int row, int col) {
         checkRange(row, col);
         return row * N + col;
     }
 
-    private void checkRange(int row, int col){
-        if (row < 0 || row >= N){
+    private void checkRange(int row, int col) {
+        if (row < 0 || row >= N) {
             throw new java.lang.IllegalArgumentException("row out of range");
         }
-        if (col < 0 || col >= N){
+        if (col < 0 || col >= N) {
             throw new java.lang.IllegalArgumentException("col out of range");
         }
     }
 
     // open the site (row, col) if it is not open already
-    public void open(int row, int col){
+    public void open(int row, int col) {
         checkRange(row, col);
         if (!isOpen(row, col)) {
             grid[row][col] = 1;
@@ -51,24 +52,56 @@ public class Percolation {
     }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col){
+    public boolean isOpen(int row, int col) {
         checkRange(row, col);
         return grid[row][col] > 0; //open, full
     }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col){
+    public boolean isFull(int row, int col) {
         checkRange(row, col);
         return grid[row][col] == 2;
     }
 
     // number of open sites
-    public int numberOfOpenSites(){
+    public int numberOfOpenSites() {
         return nOfOpenSites;
     }
 
     // does the system percolate?
-    public boolean percolates(){
+    public boolean percolates() {
+        for (int j = 0; j < N; j++) {
+            if (isOpen(0, j)) {
+                grid[0][j] = 2;
+                percolationSimulation(0, j);
+            }
+        }
+        for (int j = 0; j < N; j++) {
+            if (isFull(N - 1, j)) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    private void percolationSimulation(int row, int col) {
+        for (int i = 0; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (!(i == 0 && j == 0)) {
+                    try {
+                        checkRange(row + i,col + j);
+                    } catch (IllegalArgumentException e){
+                        continue;
+                    }
+                    if (isFull(row + i, col + j)) {
+                        continue;
+                    }
+                    if (isOpen(row + i, col + j)) {
+                        grid[row + i][col + j] = 2;
+                        percolationSimulation(row + i, col + j);
+                    }
+                }
+            }
+        }
     }
 }
--- hw2/hw2/testPercolation.java
+++ hw2/hw2/testPercolation.java
@@ -1,10 +1,33 @@
 package hw2;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class testPercolation {
     @Test
-    public void test(){
-
+    public void testGeneral(){
+        Percolation p =new Percolation(3);
+        assertEquals(0,p.numberOfOpenSites());
+        assertEquals(false, p.isFull(0,0));
+        assertEquals(false, p.isOpen(0,0));
+        assertEquals(false, p.percolates());
+        p.open(0,0);
+        assertEquals(true, p.isOpen(0,0));
+        p.open(1,0);
+        assertEquals(true, p.isOpen(1,0));
+        p.open(2,0);
+        assertEquals(true, p.isOpen(2,0));
+        assertEquals(true, p.percolates());
+    }
+    @Test
+    public void testPercolates(){
+        Percolation p3 =new Percolation(3);
+        assertEquals(false, p3.percolates());
+        p3.open(0,0);
+        p3.open(1,0);
+        p3.open(1,1);
+        p3.open(2,1);
+        p3.open(2,2);
+        assertEquals(true, p3.percolates());
     }
 }
