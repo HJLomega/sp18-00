d562691342a5a54e6e1c3ea4af3ce5bf0179def3
ljh <2450642014@qq.com>
2022-09-11 10:52:22 +0800

    complete Percolation

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -5,9 +5,11 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
 
     private int N;
-    //private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF uf;
     private int nOfOpenSites;
-    private int[][] grid;
+    private boolean[][] grid;
+    private final int top;
+    private final int down;
 
     // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
@@ -15,12 +17,16 @@ public class Percolation {
             throw new java.lang.IllegalArgumentException("N must be positive");
         }
         this.N = N;
+        top = N * N;
+        down = top + 1;
         nOfOpenSites = 0;
-        //uf = new WeightedQuickUnionUF(N * N);
-        grid = new int[N][N];
+
+        uf = new WeightedQuickUnionUF(N * N + 2);// N * N :top , N * N + 1 down
+
+        grid = new boolean[N][N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                grid[i][j] = 0;
+                grid[i][j] = false;
             }
         }
     }
@@ -43,111 +49,66 @@ public class Percolation {
     public void open(int row, int col) {
         checkRange(row, col);
         if (!isOpen(row, col)) {
-            grid[row][col] = 1;
+            grid[row][col] = true;
             nOfOpenSites += 1;
-            newToBeFull(row, col);
+            if (row == 0) {
+                connectAround(row, col);
+                uf.union(top, xy2Index(row, col));
+                return;
+            }
+            if (row == N - 1) {
+                connectAround(row, col);
+                uf.union(down, xy2Index(row, col));
+                return;
+            }
+            connectAround(row, col);
         }
     }
 
-
-    /* check whether a new opened site should be full, if so set that site full */
-    private void newToBeFull(int row, int col) {
-        if (row == 0) {
-            setFull(row, col, row - 1, col);
-            return;
-        }
-        boolean toBeFull;
+    private void connectAround(int row, int col) {
+        boolean opened;
         try {
-            toBeFull = isFull(row - 1, col);
-            if (toBeFull) {
-                setFull(row, col, row - 1, col);
-                return;
+            opened = isOpen(row - 1, col);
+            if (opened) {
+                uf.union(xy2Index(row, col), xy2Index(row - 1, col));
             }
         } catch (IndexOutOfBoundsException e) {
         }
         try {
-            toBeFull = isFull(row + 1, col);
-            if (toBeFull) {
-                setFull(row, col, row + 1, col);
-                return;
+            opened = isOpen(row + 1, col);
+            if (opened) {
+                uf.union(xy2Index(row, col), xy2Index(row + 1, col));
             }
         } catch (IndexOutOfBoundsException e) {
         }
         try {
-            toBeFull = isFull(row, col - 1);
-            if (toBeFull) {
-                setFull(row, col, row, col - 1);
-                return;
+            opened = isOpen(row, col - 1);
+            if (opened) {
+                uf.union(xy2Index(row, col), xy2Index(row, col - 1));
             }
         } catch (IndexOutOfBoundsException e) {
         }
         try {
-            toBeFull = isFull(row, col + 1);
-            if (toBeFull) {
-                setFull(row, col, row, col + 1);
-                return;
+            opened = isOpen(row, col + 1);
+            if (opened) {
+                uf.union(xy2Index(row, col), xy2Index(row, col + 1));
             }
         } catch (IndexOutOfBoundsException e) {
         }
-
-    }
-
-    /* check whether a full site should FlowAround, if so set the corresponded site full */
-    private void fullFlowAround(int row, int col) {
-        boolean toBeFull;
-        try {
-            toBeFull = isOpen(row - 1, col);
-            if (toBeFull) {
-                setFull(row - 1, col, row, col);
-            }
-        } catch (IndexOutOfBoundsException e) {
-        }
-        try {
-            toBeFull = isOpen(row + 1, col);
-            if (toBeFull) {
-                setFull(row + 1, col, row, col);
-            }
-        } catch (IndexOutOfBoundsException e) {
-        }
-        try {
-            toBeFull = isOpen(row, col - 1);
-            if (toBeFull) {
-                setFull(row, col - 1, row, col);
-            }
-        } catch (IndexOutOfBoundsException e) {
-        }
-        try {
-            toBeFull = isOpen(row, col + 1);
-            if (toBeFull) {
-                setFull(row, col + 1, row, col);
-            }
-        } catch (IndexOutOfBoundsException e) {
-        }
-    }
-
-    /* connect source and the input site,
-        check whether the site below  should be open
-     */
-    private void setFull(int row, int col, int sourceRow, int sourceCol) {
-        if (isFull(row, col)) {
-            return;
-        }
-        grid[row][col] = 2;
-        //uf.union(xy2Index(row, col), xy2Index(sourceRow, sourceCol));
-        fullFlowAround(row, col);
     }
 
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
         checkRange(row, col);
-        return grid[row][col] > 0; //open, full
+        return grid[row][col];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         checkRange(row, col);
-        return grid[row][col] == 2;
+        int top = N * N;
+        return uf.connected(xy2Index(row, col), top);
     }
 
     // number of open sites
@@ -157,11 +118,6 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        for (int j = 0; j < N; j++) {
-            if (isFull(N - 1, j)) {
-                return true;
-            }
-        }
-        return false;
+        return uf.connected(top, down);
     }
 }
