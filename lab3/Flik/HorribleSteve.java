6334abadb295fb998603fd0df24ef29217aa1b69
ljh <2450642014@qq.com>
2022-09-19 22:04:21 +0800

    proxy test

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,7 +1,16 @@
+
 public class HorribleSteve {
     public static void main(String [] args) {
+        //System.out.println(Flik.isSameNumber(128, 128));
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
+            /* debug
+            if ((i == j) != Flik.isSameNumber(i, j)){
+                System.out.print(i);
+                System.out.print(j);
+                System.out.println("error");
+            }
+            */
             if (!Flik.isSameNumber(i, j)) {
                 break; // break exits the for loop!
             }
